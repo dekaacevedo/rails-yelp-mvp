afe3ce8851c91d8d33a02f6e4b4080a3c489f8d7
Andrea Acevedo <andreaacevedo6@gmail.com>
2021-07-30 16:53:02 -0400

    correcciones en migracion y validaciones

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,6 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  validates :phone_number, presence: true
+  validates :name, :address, :phone_number, :category, presence: true
+  validates :category, inclusion: { in: %w(chinese italian japanese french belgian) }
 end
