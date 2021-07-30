04ee6560c38b4490d0d29234bd2a74c6908be966
Andrea Acevedo <andreaacevedo6@gmail.com>
2021-07-30 17:19:22 -0400

    routing ready

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Llamamos todas las rutas para restaurants
+  resources :restaurants, only: %i[index show create new] do
+    # Hacemos nested (review de 1 restaurant) de la rutas new y create de review
+    resources :reviews, only: %i[new create]
+  end
+  # Llamamos por fuera del nested, porque no necesitamos que dependa del restaurant para eliminar
+  # resources :reviews, only: %i[destroy]
 end
